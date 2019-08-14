cc7e211f97a708e296c1eb9b76daa592ca0f3bbe
Daniel Main <daniel.main@protonmail.com>
2019-08-15 00:01:05 +0200

    Removed unused libraries

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = {
   presets: [['@babel/preset-env'], ['@babel/preset-react']],
   plugins: [
     '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-react-jsx',
     'transform-remove-console',
     'transform-flow-strip-types',
     [
--- src/components/statusalert/StatusAlert.jsx
+++ src/components/statusalert/StatusAlert.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import _ from 'lodash';
+import { dateTime } from 'node-datetime';
 import type Status from '../../types/Status.js.flow';
 
 type StatusAlertProps = {
@@ -13,9 +14,15 @@ const StatusAlert = (props: StatusAlertProps) => {
 
   useEffect(() => {
     if (!_.isEmpty(status)) {
-      statusContent.push(status);
-      const newStatusContent = _.map(statusContent, (st) => ({ ...st, open: true }));
-      setStatusContent(newStatusContent);
+      if (!statusContent || _.isEmpty(statusContent)) {
+        setStatusContent([{ ...status, open: true }]);
+      } else {
+        const newStatusContent = _.map(statusContent, (st) => ({ ...st, open: true }));
+        if (!_.find(statusContent, ['timestamp', status.timestamp])) {
+          newStatusContent.push({ ...status, open: true });
+        }
+        setStatusContent(newStatusContent);
+      }
     }
   }, [status]);
 
@@ -25,7 +32,7 @@ const StatusAlert = (props: StatusAlertProps) => {
   };
 
   return (
-    <div>
+    <Fragment>
       { _.map(statusContent, (st) => (
         <Snackbar
           key={st.timestamp}
@@ -42,7 +49,7 @@ const StatusAlert = (props: StatusAlertProps) => {
           message={<span id={st.timestamp}>{st.msg}</span>}
         />
       ))}
-    </div>
+    </Fragment>
   );
 };
 
